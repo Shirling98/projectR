6c915c5344c68fcf4904e39f53a7f627a7568096
Shirling98 <shirling.rita@gmail.com>
2024-11-05 10:40:00 +0700

    add ts

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -5,16 +5,22 @@ const {
 const uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
 const map = require('gulp-sourcemaps');
+const ts = require('gulp-typescript');
 const browserSync = require('browser-sync').create();
 
 
 module.exports = function scripts() {
     return src([
-        'app/js/main.js'
+        'app/scripts/*.js', 'app/scripts/*.ts'
     ])
-        .pipe(concat('main.min.js'))
-        .pipe(map.write('../sourcemaps'))
+        .pipe(map.write('../sourcemaps'))    
+        .pipe(ts({
+            noImplicitAny: true,
+            outFile: 'main.min.js',
+            allowJs: true 
+        }))   
         .pipe(uglify())
+        .pipe(concat('main.min.js'))
         .pipe(dest('app/js'))
         .pipe(browserSync.stream());
     }
